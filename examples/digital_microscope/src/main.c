27e0ec3c7f741911d8ffa16aa91fdf561c3a2d65
Tobias Kaestner <tobias.kaestner@tiac-systems.net>
2024-09-11 10:01:12 +0200

    feat: add example skeleton for digital microscope
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

---
+++ examples/digital_microscope/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024 Carl Zeiss Meditec AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/logging/log.h>
+#include <stdlib.h>
+
+LOG_MODULE_REGISTER(app);
+
+int main(void)
+{
+
+	return 0;
+}
